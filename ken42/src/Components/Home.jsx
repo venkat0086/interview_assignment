5fe7ab9f3621e59f602703a1d2cc7ab0c5bb1f76
Venkat R <95846720+venkat0086@users.noreply.github.com>
2022-06-08 16:28:12 +0530

    updated

--- ken42/src/Components/Home.jsx
+++ ken42/src/Components/Home.jsx
@@ -1,42 +1,103 @@
 import "../Styles/Home.css";
-// import { Link } from "react-router-dom";
-// import axios from "axios";
-// import { useEffect, useState } from "react";
-// import paan from "../Images/paan.jpg";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { NavBar } from "./NavBar";
 export const Home = () => {
-  // const [category, setCategory] = useState([]);
-  // let storeCart = JSON.parse(localStorage.getItem("ItemsInCart")) || [];
-  // let totalSum = 0,
-  //   totalDisc = 0;
-  // storeCart.forEach((e) => {
-  //   totalSum += e.price;
-  //   totalDisc += Math.round((+e.price * e.discount) / 100);
-  // });
-  // let mrp = totalSum - totalDisc;
+  const [data, setData] = useState([]);
+  const [studentCount, setStudentCount] = useState(0);
+  useEffect(() => {
+    axios.get("https://ken42app.herokuapp.com/studentlist").then((res) => {
+      setData([...res.data]);
+      setStudentCount(res.data.length);
+    });
+  }, []);
 
-  // useEffect(() => {
-  //   axios.get("https://blinkitapp.herokuapp.com/products-image").then((res) => {
-  //     setCategory([...res.data]);
-  //   });
-  // }, []);
+  const filterByTerm = (value) => {
+    axios
+      .get(`https://ken42app.herokuapp.com/studentlist?term=${value}`)
+      .then((res) => {
+        setData(res.data);
+      });
+  };
+
+  const filterByCurrYear = (value) => {
+    axios
+      .get(`https://ken42app.herokuapp.com/studentlist?curyear=${value}`)
+      .then((res) => {
+        setData(res.data);
+      });
+  };
+
+  const checkForYear = (num) => {
+    return num === 1
+      ? num + "st"
+      : num === 2
+      ? num + "nd"
+      : num === 3
+      ? num + "rd"
+      : num + "th";
+  };
 
   return (
     <div>
       <NavBar />
       <div className="main-container">
-        {/* <div className="paan-img">
-          <img src={paan} alt="paan corner" width="100%" />
+        <div className="term-head">
+          <div>Filter</div>
+          <div>
+            Term:{" "}
+            <select
+              onChange={(e) => {
+                filterByTerm(e.target.value);
+              }}
+            >
+              <option value="">Select</option>
+              <option value="PUC">PUC</option>
+              <option value="UG">Under Graduate</option>
+              <option value="PG">Post Graduate</option>
+            </select>{" "}
+            <span>
+              Current Year:{" "}
+              <select
+                onChange={(e) => {
+                  filterByCurrYear(e.target.value);
+                }}
+              >
+                <option value="">Select</option>
+                <option value="1">First Year</option>
+                <option value="2">Second Year</option>
+                <option value="3">Third Year</option>
+              </select>
+              <span>
+                <button>Total Students: {studentCount}</button>
+              </span>
+            </span>
+          </div>
         </div>
-        <div className="category-main-img">
-          {category.map((e) => (
-            <div key={e._id}>
-              <Link to={`/products`}>
-                <img src={e.image} alt={e.alt} />
-              </Link>
-            </div>
-          ))}
-        </div> */}
+        <div className="container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Roll Number</th>
+                <th>Term</th>
+                <th>Current Year</th>
+                <th>Contact Number</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((e) => (
+                <tr key={e._id}>
+                  <td>{e.name}</td>
+                  <td>{e.rollno}</td>
+                  <td>{e.term}</td>
+                  <td>{`${checkForYear(e.currentyear)} year`}</td>
+                  <td>{e.contactno}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- ken42/src/Components/NavBar.jsx
+++ ken42/src/Components/NavBar.jsx
@@ -14,15 +14,6 @@ export const NavBar = (props) => {
           />
         </Link>
       </div>
-      {/* <div className="location-main">
-        <div>
-          <div className="nav-container-heading">Delivery in 25 minutes</div>
-          <div className="nav-container-sub-heading">Rajajinagar,Bengaluru</div>
-        </div>
-        <div>
-          <GrLocation className="location-logo" />
-        </div>
-      </div> */}
       <div className="search-main">
         <button className="search-btn">
           <GoSearch />
@@ -43,24 +34,6 @@ export const NavBar = (props) => {
           Admin Login
         </Link>
       </div>
-      {/* <div className="cart-main">
-        <button
-          className="cart-btn"
-          onClick={() => {
-            navigate("/cart");
-          }}
-        >
-          <div className="cart-div">
-            <div>
-              <BsCart3 />
-            </div>
-            <div>
-              <Link to="/register" />
-              {`${cartCheck(props.cart)}`}
-            </div>
-          </div>
-        </button>
-      </div> */}
     </div>
   );
 };
